97eae941ddae56e1e8735818cfdf16efde9ecbb3
Wyatt Baldwin <self@wyattbaldwin.com>
2017-07-27 10:27:15 -0700

    [setup.sh] Fix/change quoting of variables
    - Add quotes around all strings with embedded vars
    - Remove brackets around vars in strings that contain just a single var:
      i.e., change "${xyz}" to "$xyz"

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ function link () {
     #     $1: path relative to file in dot files repo
     #     $2: target path (optional; default is $HOME/.$1 or $HOME/$1)
     local file="${REPO_DIR}/${1}"
-    if [ ! -f "${file}" ]; then
+    if [ ! -f "$file" ]; then
         echo "${RED}${file} does not exist in .files repo${RESET}" 1>&2
         return 1
     fi
@@ -32,18 +32,18 @@ function link () {
         local target="${2}"
     else
         local target="${HOME}/.${1}"
-        if [ ! -d "$(dirname ${target})" ]; then
+        if [ ! -d "$(dirname "$target")" ]; then
             local target="${HOME}/${1}"
         fi
     fi
-    local target_dir="$(dirname ${target})"
-    if [ ! -d "${target_dir}" ]; then
+    local target_dir="$(dirname "$target")"
+    if [ ! -d "$target_dir" ]; then
         echo "${RED}Target directory \"${target_dir}\" does not exist${RESET}" 1>&2
         return 1
     fi
     if [ ! -L "$target" ]; then
-        save_original $target
-        ln -s $file $target
+        save_original "$target"
+        ln -s "$file" "$target"
         echo "${GREEN}Linked ${target} to ${file}${RESET}"
     else
         echo "${YELLOW}${target} already points to $(readlink ${target})${RESET}" 1>&2
@@ -82,11 +82,11 @@ link vimrc
 link ssh/config
 
 for file in "${REPO_DIR}/bashrc.d/"*.rc; do
-    link "bashrc.d/$(basename "${file}")"
+    link "bashrc.d/$(basename "$file")"
 done
 
 for file in "${REPO_DIR}/local/bin/"*; do
-    link "local/bin/$(basename "${file}")"
+    link "local/bin/$(basename "$file")"
 done
 
 test -d ~/Library/LaunchAgents && link Library/LaunchAgents/gpg-agent.plist
@@ -94,12 +94,12 @@ test -d ~/Library/LaunchAgents && link Library/LaunchAgents/gpg-agent.plist
 if [ "$(uname -s)" = "Darwin" ]; then
     # Install Homebrew & some packages
     brew_path="/usr/local/bin/brew"
-    if [ -f $brew_path ]; then
+    if [ -f "$brew_path" ]; then
         echo "${YELLOW}Homebrew already installed at prefix $($brew_path --prefix)${RESET}"
     else
         ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        $brew_path doctor
-        $brew_path install \
+        "$brew_path" doctor
+        "$brew_path" install \
             ack \
             bash-completion \
             gpg2 \
@@ -135,7 +135,7 @@ function install_pip () {
     else
         get_pip_installer
         echo -n "${BLUE}Installing pip for Python ${version}... "
-        ${python_exe} get-pip.py --upgrade --force-reinstall
+        "$python_exe" get-pip.py --upgrade --force-reinstall
         if which pyenv >/dev/null 2>&1; then
             pyenv rehash
         fi
@@ -156,13 +156,13 @@ pip3 install -U checkoutmanager >/dev/null
 pip3 install -U virtualenv >/dev/null
 echo "${GREEN}Done${RESET}"
 
-mkdir -p ${HOME}/.vim/{autoload,bundle}
+mkdir -p "${HOME}/.vim/{autoload,bundle}"
 echo -n "${BLUE}Checking out Pathogen plugins... "
 checkoutmanager co vim-pathogen >/dev/null
 echo "${GREEN}Done${RESET}"
 pathogen_path="${HOME}/.vim/vim-pathogen/autoload/pathogen.vim"
 pathogen_link="${HOME}/.vim/autoload/pathogen.vim"
-if [ -L $pathogen_link ]; then
+if [ -L "$pathogen_link" ]; then
     echo "${YELLOW}pathogen.vim already linked to $(readlink $pathogen_link)${RESET}"
 else
     echo -n "${BLUE}Linking ${pathogen_link} to ${pathogen_path}... "
