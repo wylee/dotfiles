df9ed739718dde5bda4c46060ef24381b8bf316f
Wyatt Baldwin <self@wyattbaldwin.com>
2018-02-01 22:20:40 -0800

    [fish] Start tmux automatically (if it's installed)

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,15 +1,28 @@
-fish_vi_key_bindings
+function __init
+    fish_vi_key_bindings
 
-set -g -x PROJECT_DIR ~/Projects
+    set -g -x PROJECT_DIR ~/Projects
 
-set -g -x fish_prompt_date_format '+%a %b %d %T'
-set -g -x fish_greeting "Shell started" (date $fish_prompt_date_format)
-set -g -x EDITOR vim
+    set -g -x fish_prompt_date_format '+%a %b %d %T'
+    set -g -x fish_greeting "Shell started" (date $fish_prompt_date_format)
+    set -g -x EDITOR vim
 
-set script_path (status -f)
-set script_dir (dirname $script_path)
-for f in $script_dir/*.fish
-    if [ $f != $script_path ]
-        source "$f"
+    set script_path (status -f)
+    set script_dir (dirname $script_path)
+    for f in $script_dir/*.fish
+        if [ $f != $script_path ]
+            source "$f"
+        end
+    end
+end
+
+if [ "$TERM" = "screen" ]
+    __init
+else
+    if which -s tmux
+        echo "Starting tmux..."
+        exec tmux
+    else
+        __init
     end
 end
