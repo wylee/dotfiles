806c61105c70640cff58d969720472b10a8f4ccb
Wyatt Baldwin <self@wyattbaldwin.com>
2018-02-05 10:46:03 -0800

    [fish] Add pyenv-install function

--- config/fish/pyenv.fish
+++ config/fish/pyenv.fish
@@ -4,3 +4,25 @@ set -gx PYENV_ROOT (brew --prefix)/var/pyenv
 # This sets up pyenv shims and bash completion; run `pyenv init -` to
 # see everything it does.
 status --is-interactive; and source (pyenv init -|psub)
+
+function pyenv-install
+    set cppflags
+    set ldflags
+    set configure_opts "--enable-shared"
+
+    for lib in openssl readline zlib
+        set cppflags $cppflags "-I"(brew --prefix $lib)/include
+        set ldflags $ldflags "-L"(brew --prefix $lib)/lib
+    end
+
+    begin
+        set -lx CPPFLAGS "$cppflags"
+        set -lx LDFLAGS "$ldflags"
+        set -lx PYTHON_CONFIGURE_OPTS "$configure_opts"
+        echo "Running `pyenv install $argv` with:"
+        echo "CPPFLAGS=$CPPFLAGS"
+        echo "LDFLAGS=$LDFLAGS"
+        echo "PYTHON_CONFIGURE_OPTS=$PYTHON_CONFIGURE_OPTS"
+        pyenv install $argv
+    end
+end
