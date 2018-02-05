720ac92fae7a0f7224db8bd1d3f10f4e30744167
Wyatt Baldwin <self@wyattbaldwin.com>
2018-02-05 10:44:43 -0800

    [fish] Improve pyenv initialization
    pyenv provides an init script for fish, so use that instead of trying to
    set things up manually.

--- config/fish/pyenv.fish
+++ config/fish/pyenv.fish
@@ -1,3 +1,6 @@
-set PYENV_ROOT (brew --prefix)/var/pyenv
-__prepend_path $PYENV_ROOT/shims
-pyenv rehash
+# PYENV_ROOT is where Python versions will be installed
+set -gx PYENV_ROOT (brew --prefix)/var/pyenv
+
+# This sets up pyenv shims and bash completion; run `pyenv init -` to
+# see everything it does.
+status --is-interactive; and source (pyenv init -|psub)
