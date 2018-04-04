9c70250ee092982d515b04e8efc489b9f73af0e0
Wyatt Baldwin <self@wyattbaldwin.com>
2018-04-03 19:00:31 -0700

    [fish/hosts] Tweak script for uploading blocked hosts to router

--- config/fish/functions/make-hosts-blackhole.fish
+++ config/fish/functions/make-hosts-blackhole.fish
@@ -10,5 +10,6 @@ function make-hosts-blackhole
     sed -En 's!^0\.0\.0\.0 (.+)$!address=/\1/0.0.0.0!p' $hosts >$dnsmasq_hosts
 
     scp $dnsmasq_hosts $router_ip:/config/user-data/$dnsmasq_hosts
+    ssh $router_ip sudo cp /config/user-data/$dnsmasq_hosts /etc/dnsmasq.d
     ssh $router_ip sudo /etc/init.d/dnsmasq force-reload
 end
