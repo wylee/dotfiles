1dd530c8c3a5e30779b0006fe1aadb950ef2ce51
Wyatt Baldwin <self@wyattbaldwin.com>
2020-03-29 13:28:20 -0700

    [fish] Improve $PATH setup
    - Prepend `/usr/local/bin`
    - Prepend system paths like `/usr/local/bin` before sourcing other fish
      config files (in case they rely on those paths)
    - Prepend `~/.local/bin` after sourcing other fish config files; this is
      to ensure `~/.local/bin` is first in `$PATH`

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,6 +9,9 @@ function __init
         set -gx fish_greeting "Shell started" (date $fish_prompt_date_format)
     end
 
+    __prepend_path /usr/local/bin
+    __prepend_path /usr/local/opt/postgresql@9.4/bin
+
     set script_path (status -f)
     set script_dir (dirname $script_path)
     for f in $script_dir/*.fish
@@ -17,13 +20,12 @@ function __init
         end
     end
 
-    __prepend_path /usr/local/opt/postgresql@9.4/bin
     __prepend_path ~/.local/bin
 end
 
 function __prepend_path -a path -d "Add path to front of PATH if it's not already in PATH"
     if test -d $path; and not string match -qr $path $PATH
-        set -gx PATH $path $PATH
+        set --prepend PATH $path
     end
 end
 
