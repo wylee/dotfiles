e5e4da67a552f00d0583f69118c669e3e505a92f
Wyatt Baldwin <self@wyattbaldwin.com>
2020-12-26 17:24:37 -0800

    [fish] Improve prompt
    The main change is putting the env and VCS info on a separate line. In
    addition, the VCS info is now prefixed with "git:" or "hg:".

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,24 +1,45 @@
 set -g fish_prompt_pwd_dir_length 0
 
 function fish_prompt
+    # First line: date and time
     set_color blue
     echo (date $fish_prompt_date_format)
 
+    # Second line: user and host
     set_color red
-    echo -n $USER
+    echo -n "$USER"
     set_color yellow
-    echo -n @
+    echo -n "@"
     set_color green
     echo (prompt_hostname)
 
+    # Third line: PWD
     set_color cyan
-    echo -n (prompt_pwd)
-    echo -n (__fish_vcs_prompt)
-    if set -q _ENV_CURRENT
-        echo -n " (env:"(basename $_ENV_CURRENT)")"
-    end
-    echo
+    echo (prompt_pwd)
 
+    # Fourth line: env and VCS info
+    set vcs_prompt (__fish_vcs_prompt)
+    set vcs_prompt (string trim -l -c ' (' "$vcs_prompt")
+    set vcs_prompt (string trim -r -c ' )' "$vcs_prompt")
+
+    if test -n "$vcs_prompt"
+        if git rev-parse --show-toplevel 1>/dev/null 2>/dev/null
+            set vcs_prompt "git:$vcs_prompt"
+        else if hg root 1>/dev/null 2>/dev/null
+            set vcs_prompt "hg:$vcs_prompt"
+        else
+            set vcs_prompt "vcs:$vcs_prompt"
+        end
+    end
+
+    if set -q _ENV_CURRENT
+        echo -n "env:"(basename $_ENV_CURRENT)
+        test -n "$vcs_prompt"; and echo " - $vcs_prompt"; or echo
+    else if test -n "$vcs_prompt"
+        echo "$vcs_prompt"
+    end
+
+    # Fifth line: prompt
     set_color yellow
     echo -n "> "
 
