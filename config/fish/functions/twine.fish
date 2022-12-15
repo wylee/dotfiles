6cb4e5f2cea78bea27c60dbb824b1c85e97ea3a4
Wyatt Baldwin <self@wyattbaldwin.com>
2022-12-15 15:11:23 -0800

    [fish] Update twine function to assume tokens are used for uploads

--- config/fish/functions/twine.fish
+++ config/fish/functions/twine.fish
@@ -1,11 +1,41 @@
 # Inspired by https://glyph.twistedmatrix.com/2017/10/careful-with-that-pypi.html
+#
+# This assumes that credentials are stored in ~/.pypirc by default.
+#
+# PyPI tokens should be used for all uploads. These tokens are created
+# at https://pypi.org/manage/account/token/, and then added to
+# ~/.pypirc, like so:
+#
+# [distutils]
+# index-servers =
+#     runcommands
+#
+# [runcommands]
+# repository = https://upload.pypi.org/legacy/
+# username = __token__
+# password = pypi-<token>
+#
+# Usage:
+#     1. `poetry build` (or `python setup.py sdist`, etc)
+#     2. `twine upload dist/*`
 function twine
-    # Usage:
-    #     1. python setup.py sdist
-    #     2. twine upload dist/*
+    set -l twine_args $argv
+    set -l options "r/repository="
+    argparse --name="twine" $options -- $argv or return
+
     begin
-        set -lx TWINE_USERNAME (bw get username PyPI)
-        set -lx TWINE_PASSWORD (bw get password PyPI)
+        if set -q _flag_repository
+            echo "PyPI repository: $_flag_repository"
+        else
+            set -x TWINE_REPOSITORY (basename (pwd))
+            echo "PyPI repository (derived from directory name): $TWINE_REPOSITORY"
+            echo
+            echo "NOTE: You can use the -r/--repository option to override this"
+        end
+
+        echo
+        echo "> twine $twine_args"
+
         command twine $argv
     end
 end
