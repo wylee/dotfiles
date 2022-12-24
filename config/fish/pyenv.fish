235a359f3dc4ae048976b23a1be1605d9f492176
Wyatt Baldwin <self@wyattbaldwin.com>
2022-12-23 19:04:09 -0800

    [fish, pyenv] Tweak pyenv-install wrapper script

--- config/fish/pyenv.fish
+++ config/fish/pyenv.fish
@@ -9,14 +9,20 @@ status is-login; and pyenv init --path | source
 pyenv init - | source
 
 function pyenv-install
+    set -l zlib_prefix (brew --prefix zlib)
+
     begin
-        set -lx CPPFLAGS "-I$BREW_PREFIX/opt/zlib/include"
-        set -lx LDFLAGS "-L$BREW_PREFIX/opt/zlib/lib"
+        set -lx CPPFLAGS "-I$zlib_prefix/include"
+        set -lx LDFLAGS "-L$zlib_prefix/lib"
         set -lx PYTHON_CONFIGURE_OPTS "--enable-shared"
+
         echo "Running `pyenv install $argv` with:"
-        echo "CPPFLAGS=$CPPFLAGS"
-        echo "LDFLAGS=$LDFLAGS"
-        echo "PYTHON_CONFIGURE_OPTS=$PYTHON_CONFIGURE_OPTS"
+        echo
+        echo "  CPPFLAGS=$CPPFLAGS"
+        echo "  LDFLAGS=$LDFLAGS"
+        echo "  PYTHON_CONFIGURE_OPTS=$PYTHON_CONFIGURE_OPTS"
+        echo
+
         pyenv install $argv
     end
 end
